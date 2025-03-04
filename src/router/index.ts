542d20e7a51c75c7697695b16d94ec001374350a
alberba <83814585+alberba@users.noreply.github.com>
2025-03-04 15:38:16 +0100

    Agregar autenticación en rutas: redirigir a LoginPage si no está autenticado

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,22 @@ import LoginView from '../views/LoginPage.vue'
 import HomePage from '@/views/HomePage.vue'
 
 const routes = [
-  { path: '/', component: LoginView },
-  { path: '/home', component: HomePage },
+  { path: '/', component: HomePage, meta: { requiresAuth: true } },
+  { path: '/login', component: LoginView },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+// Guard de navegación para verificar la autenticación
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = false // Aquí deberías implementar tu lógica de autenticación
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login')
+  }
+  next()
 })
 
 export default router
