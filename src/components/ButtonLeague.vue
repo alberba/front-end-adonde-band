a8a13f6b5301cbb76297096e534ecaa1f711b9f8
alberba <albertsalomv@gmail.com>
2025-03-04 14:20:42 +0100

    Agregar componente ButtonLeague.vue para mostrar información de ligas y ajustar estilos en HeaderApp.vue

---
+++ src/components/ButtonLeague.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { Liga } from '@/types'
+import { computed } from 'vue'
+const props = defineProps<{ liga: Liga }>()
+const liga = props.liga
+
+const imageClass = computed(() => {
+  const img = new Image()
+  img.src = props.liga.imagePath
+  return img.width > img.height ? 'w-full' : 'h-full'
+})
+</script>
+
+<template>
+  <div class="w-fit">
+    <div
+      class="flex h-[145px] w-[250px] items-center justify-center rounded-t-xl bg-white px-[40px] py-[12px]"
+    >
+      <img :src="liga.imagePath" :alt="liga.nombre" :class="imageClass" class="object-contain" />
+    </div>
+    <h3 class="flex justify-center w-full rounded-b-xl bg-[#525252] py-3 px-2 text-xl">{{ liga.nombre }}</h3>
+  </div>
+</template>
--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="mx-12 my-2.5 flex flex-row items-center justify-between">
+  <header class="px-12 py-2.5 flex flex-row items-center justify-between w-full">
     <h2 class="text-[24px]">¡Hola, AdondeBoy!</h2>
     <nav class="flex flex-row items-center gap-11">
       <div class="font-semibol overflow-hidden rounded-full bg-[#525252] px-5 py-3 leading-none">
