f47d1fd339aea45f6f5931d255102d7429570aad
alberba <83814585+alberba@users.noreply.github.com>
2025-03-05 18:58:54 +0100

    Add Combate interface and create SectionSideBar component

---
+++ src/components/SideBar/SectionSideBar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ButtonChat from './ButtonChat.vue'
+import type { Combate } from '@/types'
+
+const props = defineProps<{ title: string; combates?: Combate[] }>()
+</script>
+
+<template>
+  <section class="flex flex-col items-start gap-2">
+    <div>
+      <h4>{{ props.title }}</h4>
+      <div class="border-b"></div>
+    </div>
+    <ButtonChat v-for="combate in props.combates" :key="combate.id">{{ combate.fighter1 }} vs {{ combate.fighter2 }}</ButtonChat>
+  </section>
+</template>
