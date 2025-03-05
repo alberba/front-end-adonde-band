011e699bcdac3ed1a1d0c503d3f21221784fbf25
alberba <83814585+alberba@users.noreply.github.com>
2025-03-05 18:39:44 +0100

    Create ButtonChat Component

---
+++ src/components/ButtonChat.vue
@@ -0,0 +1,3 @@
+<template>
+  <button class="px-3 py-1 max-w-full text-lg hover:rounded-2xl hover:bg-[#4a4a4a] whitespace-nowrap overflow-ellipsis overflow-hidden"><slot></slot></button>
+</template>
