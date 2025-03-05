f1aa6a7471c0f20afcf53aa6d4dc254f0175e479
alberba <83814585+alberba@users.noreply.github.com>
2025-03-05 13:59:28 +0100

    Creado componente MessageApp

---
+++ src/components/MessageApp.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+const props = defineProps<{ id: number; imgPath: string }>()
+
+const isTextLeft = props.id % 2 === 0
+const bgColor = !isTextLeft ? 'bg-[#06f]' : 'bg-[#606060]'
+const radiusClass = ['rounded-3xl', !isTextLeft ? 'rounded-br-none' : 'rounded-bl-none']
+
+function getAssetUrl(fileName: string) {
+  return new URL(`../assets/${fileName}`, import.meta.url).href
+}
+</script>
+
+<template>
+  <div class="flex items-end gap-2" :class="isTextLeft ? 'justify-start' : 'justify-end'">
+    <img
+      v-if="isTextLeft"
+      :src="getAssetUrl(props.imgPath)"
+      alt="Profile"
+      class="w-8 object-contain rounded-full"
+    />
+    <div :class="[bgColor, radiusClass]" class="max-w-xl p-3.5"><slot class=""></slot></div>
+    <img
+      v-if="!isTextLeft"
+      :src="getAssetUrl(props.imgPath)"
+      alt="Profile"
+      class="w-8 object-contain rounded-full"
+    />
+  </div>
+</template>
