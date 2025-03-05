7eb381ee39a18e6db5b2c8a45a140d443af48068
alberba <83814585+alberba@users.noreply.github.com>
2025-03-05 18:58:54 +0100

    Add Combate interface and create SectionSideBar component

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,20 @@
 export interface Liga {
-  id: number;
-  nombre: string;
-  imagePath: string;
-  finalizado: boolean;
+  id: number
+  nombre: string
+  imagePath: string
+  finalizado: boolean
 }
 
 export interface Mensaje {
-  id: number;
-  texto: string;
-  imgPath: string;
+  id: number
+  texto: string
+  imgPath: string
+}
+
+export interface Combate {
+  id: number
+  //ligaId: number;
+  finished: boolean
+  fighter1: string
+  fighter2: string
 }
