bbe140c1a48b8e77ecfe83a969e1ce6602770f8d
alberba <albertsalomv@gmail.com>
2025-03-07 10:43:23 +0100

    Add ButtonChat hover transition

--- src/components/SideBar/ButtonChat.vue
+++ src/components/SideBar/ButtonChat.vue
@@ -1,3 +1,7 @@
 <template>
-  <button class="px-3 py-1 max-w-full text-lg hover:rounded-2xl hover:bg-[#4a4a4a] whitespace-nowrap overflow-ellipsis overflow-hidden"><slot></slot></button>
+  <button
+    class="max-w-full overflow-hidden rounded-2xl px-3 py-1 text-lg overflow-ellipsis whitespace-nowrap transition delay-75 ease-in-out hover:bg-[#4a4a4a]"
+  >
+    <slot></slot>
+  </button>
 </template>
