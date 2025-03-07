f696fbb89f90e21ff870efca377a769a525af382
alberba <83814585+alberba@users.noreply.github.com>
2025-03-07 12:45:35 +0100

    Añadir iconos de modo claro y oscuro, y permitir el cambio de modo en la barra lateral Add support for light mode SideBar

--- src/components/SideBar/ButtonChat.vue
+++ src/components/SideBar/ButtonChat.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="max-w-full overflow-hidden rounded-2xl px-3 py-1 text-lg overflow-ellipsis whitespace-nowrap transition delay-75 ease-in-out hover:bg-[#4a4a4a]"
+    class="max-w-full overflow-hidden rounded-2xl px-3 py-1 text-lg overflow-ellipsis whitespace-nowrap transition-[background-color] delay-75 ease-in-out hover:bg-[#dcdcdc] dark:hover:bg-[#4a4a4a]"
   >
     <slot></slot>
   </button>
--- src/components/SideBar/SectionSideBar.vue
+++ src/components/SideBar/SectionSideBar.vue
@@ -8,9 +8,11 @@ const props = defineProps<{ title: string; combates?: Combate[] }>()
 <template>
   <section class="flex flex-col items-start gap-2">
     <div>
-      <h4>{{ props.title }}</h4>
-      <div class="border-b"></div>
+      <h4 class="text-[#9b9b9b] dark:text-[#a8a8a8]">{{ props.title }}</h4>
+      <div class="border-b border-[#9b9b9b]"></div>
     </div>
-    <ButtonChat v-for="combate in props.combates" :key="combate.id">{{ combate.fighter1 }} vs {{ combate.fighter2 }}</ButtonChat>
+    <ButtonChat v-for="combate in props.combates" :key="combate.id"
+      >{{ combate.fighter1 }} vs {{ combate.fighter2 }}</ButtonChat
+    >
   </section>
 </template>
