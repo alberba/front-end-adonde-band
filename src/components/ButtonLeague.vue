8dafe3e3c01ac1c96a7ccb07d02da44aaca1907e
alberba <83814585+alberba@users.noreply.github.com>
2025-03-08 13:18:53 +0100

    Add Light Mode Support for HomePage

--- src/components/ButtonLeague.vue
+++ src/components/ButtonLeague.vue
@@ -14,10 +14,14 @@ const imageClass = computed(() => {
 <template>
   <div class="w-fit">
     <div
-      class="flex h-[145px] w-[250px] items-center justify-center rounded-t-xl bg-white px-[40px] py-[12px]"
+      class="flex h-[145px] w-[250px] items-center justify-center rounded-t-xl bg-white px-[40px] py-[12px] border-t-2 border-l-2 border-r-2 border-[#d3d3d3]"
     >
       <img :src="liga.imagePath" :alt="liga.nombre" :class="imageClass" class="object-contain" />
     </div>
-    <h3 class="flex justify-center w-full rounded-b-xl bg-[#525252] py-3 px-2 text-xl">{{ liga.nombre }}</h3>
+    <h3
+      class="flex w-full justify-center rounded-b-xl bg-[#d3d3d3] px-2 py-3 text-xl font-semibold dark:bg-[#525252]"
+    >
+      {{ liga.nombre }}
+    </h3>
   </div>
 </template>
