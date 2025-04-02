c4bc72feeaeeee0ea0d4900b95789cfe3de35bda
alberba <83814585+alberba@users.noreply.github.com>
2025-04-02 12:17:37 +0200

    Añadido manejo de inicio de sesión con validación y almacenamiento del nombre de usuario; se actualizó el mensaje de bienvenida en la página de inicio.

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -2,6 +2,7 @@
 import FooterApp from '@/components/FooterApp.vue'
 import { useAuthStore } from '@/store'
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
 
 const authStore = useAuthStore()
 
@@ -12,6 +13,39 @@ function tempRedirectHome() {
   // Redirecciona en caso de que haya una ruta guardada, de lo contrario redirecciona a la raíz
   router.push(redirectPath || '/')
   localStorage.removeItem('redirectPath')
+}
+
+const user = ref('')
+const password = ref('')
+
+const handleLogin = async () => {
+  const userData = {
+    user: user.value,
+    password: password.value,
+  }
+
+  try {
+    const response = await fetch('http://localhost:8080/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    })
+
+    if (!response.ok) {
+      throw new Error('Error en la solicitud')
+    }
+
+    const data = await response.json()
+    console.log('Usuario registrado:', data)
+    // Guardar el nombre de usuario en el localStorage
+    localStorage.setItem('username', user.value)
+    tempRedirectHome()
+  } catch (error) {
+    console.error('Error al iniciar sesión:', error)
+    alert('Error al iniciar sesión')
+  }
 }
 </script>
 
@@ -24,34 +58,38 @@ function tempRedirectHome() {
   <main class="my-10 flex w-full justify-center">
     <form
       action="post"
+      @submit.prevent="handleLogin()"
       class="flex w-full flex-col items-center gap-4 rounded-md bg-white p-6 text-black md:mx-24 lg:mx-36 lg:w-[700px]"
     >
-      <label for="email" class="flex w-full flex-col">
-        Email
+      <label for="user" class="flex w-full flex-col">
+        Nombre de usuario
         <input
+          v-model="user"
           class="my-2 rounded-lg border-1 border-gray-400 px-3 py-2"
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email"
+          type="text"
+          id="user"
+          name="user"
+          placeholder="Escribe aquí tu nombre de usuario"
+          required
         />
       </label>
 
       <label for="password" class="flex w-full flex-col">
         Contraseña
         <input
+          v-model="password"
           class="my-2 rounded-lg border-1 border-gray-400 px-3 py-2"
           type="password"
           id="password"
           name="password"
           placeholder="Contraseña"
+          required
         />
       </label>
 
       <a href="http://" class="self-start underline">Forgot Password?</a>
 
       <button
-        @click="tempRedirectHome()"
         type="submit"
         class="mx-3 box-border w-full rounded-full bg-[#06f] py-2 text-white md:w-xs lg:w-2xs"
       >
