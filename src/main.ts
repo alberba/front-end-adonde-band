dcbbc6b5cb40fc3bb2e964d864dbae4f6eddf24a
alberba <albertsalomv@gmail.com>
2025-04-03 11:28:42 +0200

    Añadido soporte para Vue Sweetalert2

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,13 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+const app =  createApp(App)
+
+app.use(router)
+app.use(createPinia())
+app.use(VueSweetalert2)
+
+app.mount('#app')
