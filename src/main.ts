ef58c8fa9240e77efa0c5c10b6f182d64de805ed
alberba <albertsalomv@gmail.com>
2025-04-03 12:11:53 +0200

    Implementación manejo de errores en la página de inicio de sesión.

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,11 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import { createPinia } from 'pinia'
-import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
 
 const app =  createApp(App)
 
 app.use(router)
 app.use(createPinia())
-app.use(VueSweetalert2)
 
 app.mount('#app')
